52ebf0be7a67795bdf3ce939923790f71c131b67
stewpeed <47599553+yaseenammaar@users.noreply.github.com>
2020-08-23 13:09:47 -0700

    Update UPI-Link.js

--- UPI-Link.js
+++ UPI-Link.js
@@ -1,6 +1,6 @@
-function getUPILink(vpa, name,)
+function getUPILink(vpa, name, amount, rem)
 {
   var upiLink;
-  upiLink= "upi://pay?pa=" + vpa + "&pn=" + name;
+  upiLink = "upi://pay?pa=" + vpa + "&pn=" + name + "&am="+amount+"&tn="+rem
   return upiLink;
 }
